e31702141a86c9520f08d54947c34eb8e98c9845
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 09:00:14 +0200

    classes created for creating card decks

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -20,21 +20,41 @@ namespace CardGame
 
         public void CreateDeck()
         {
+            fullDeck.Clear();
 
+            for (int suit = 0; suit < 4; suit++)
+            {
+                for (int val = 0; val < 13; val++)
+                {
+                    fullDeck.Add(new PlayingCard { Suit = (CardSuit)suit, Value = (CardValue)val });
+                }
+            }
         }
 
         public virtual void ShuffleDeck()
         {
-
+            var rnd = new Random();
+            drawPile = fullDeck.OrderBy(x => rnd.Next()).ToList();
         }
 
         public abstract List<PlayingCard> DealCard();
 
-        public virtual PlayingCard RequestCard();
+        public virtual PlayingCard RequestCard()
         {
-
+            PlayingCard output = drawPile.Take(1).First();
+            drawPile.Remove(output);
+            return output;
         }
-}
+    
+    }
+
+    public class PokerDeck : Deck
+    {
+        public override List<PlayingCard> DealCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
 
     public class PlayingCard
     {
