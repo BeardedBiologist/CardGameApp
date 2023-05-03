4cf4d719c6eedbbd93d80e98b6896143d908f117
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-03 08:41:57 +0200

    more setup

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -11,8 +11,59 @@ namespace CardGame
             Console.WriteLine();
         }
     }
+
     public abstract class Deck
     {
+        protected List<PlayingCard> fullDeck = new List<PlayingCard>();
+        protected List<PlayingCard> drawPile = new List<PlayingCard>();
+        protected List<PlayingCard> discardPile = new List<PlayingCard>();
 
+        public void CreateDeck()
+        {
+
+        }
+
+        public virtual void ShuffleDeck()
+        {
+
+        }
+
+        public abstract List<PlayingCard> DealCard();
+
+        public virtual PlayingCard RequestCard();
+        {
+
+        }
+}
+
+    public class PlayingCard
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+    }
+
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
     }
 }
